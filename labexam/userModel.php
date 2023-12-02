0b17391561826a963174d702ae7cde9cd6653e7e
Md. Kabbir Hasan <kabbirhasan009@gmail.com>
2023-12-02 12:35:55 +0600

    Add files via upload

---
+++ labexam/userModel.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+
+     $students = [
+        's1'=>['id'=>1, 'name'=>'alamin', 'cgpa'=>3.5],
+        's2'=>['id'=>2, 'name'=>'xyz', 'cgpa'=>3.5],
+        's3'=>['id'=>3, 'name'=>'abc', 'cgpa'=>3.5]
+    ];
+
+    //print('Hello PHP');
+    //echo "This is another print option";
+    //print_r($students);
+
+    for($i=0; $i<10; $i++){
+        echo "<h1>Hello PHP</h1>"." another string...";
+    }
+?>
+
+<h1>TEST</h1>
